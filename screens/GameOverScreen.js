6eac1676a2d95bd78593a1d8add3a9786e032162
Gonçalves Antoine <antoine.goncalves-isc.france@ibm.com>
2021-09-01 09:16:23 +0200

    Refact GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Text, Button, Image } from 'react-native';
+
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
+import MainButton from '../components/MainButton';
+import Colors from '../constants/color';
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over !</Text>
-      <Text>Number of rounds : {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
-      <Button title='New Game' onPress={props.onRestart} />
+      <TitleText>The Game is Over !</TitleText>
+      <Image
+        source={require('../assets/success.png')} // locally image
+        // source={{
+        //   uri: 'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_1280.jpg',
+        // }} // external image
+        style={styles.image}
+        resizeMode='cover'
+      />
+      <View style={styles.resultContainer}>
+        <BodyText style={styles.resultText}>
+          Your phone needed{' '}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+          guess the number{' '}
+          <Text style={styles.highlight}>{props.userNumber}</Text>
+        </BodyText>
+      </View>
+      <MainButton onPress={props.onRestart}>New Game</MainButton>
     </View>
   );
 };
@@ -17,6 +36,26 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: 'black',
+    marginVertical: 30,
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: 15,
+  },
+  resultText: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  highlight: {
+    color: Colors.primary,
+    fontFamily: 'open-sans-bold',
   },
 });
 
